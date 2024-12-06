6b8b801b7ed57a7df61fa7c7fb2e2f89b7a1eb16
Makovsky Dmitry <dima@makovsky.me>
2024-12-06 11:04:03 +0300

    add auto configuration for AdonisJS 6 Ally Vk Driver

---
+++ configure.ts
@@ -0,0 +1,28 @@
+/**
+ * @dmitry-makovsky/adonis6_ally_vk
+ *
+ * @license MIT
+ * @copyright Makovsky Dmitry <dima@makovsky.me>
+ */
+
+import type Configure from '@adonisjs/core/commands/configure'
+
+/**
+ * Configures the package
+ */
+export async function configure(command: Configure) {
+  const codemods = await command.createCodemods()
+
+  await codemods.defineEnvVariables({
+    VK_CLIENT_ID: '',
+    VK_CLIENT_SECRET: '',
+  })
+
+  await codemods.defineEnvValidations({
+    variables: {
+      VK_CLIENT_ID: 'Env.schema.string()',
+      VK_CLIENT_SECRET: 'Env.schema.string()',
+    },
+    leadingComment: 'Variables for @dmitry-makovsky/adonis6_ally_vk',
+  })
+}
